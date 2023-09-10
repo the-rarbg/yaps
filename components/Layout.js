50f3bc12a8855a1d1144b38d3f98ec4f768ee665
Ekaiva <Ekaiva@gmail.com>
2023-09-10 15:15:25 +0530

    first commit

--- components/Layout.js
+++ components/Layout.js
@@ -10,7 +10,7 @@ export default function Layout({ children }) {
       <main  className='mx-0 flex p-1 flex-col py-6 md:m-6 md:px-0 md:pt-0 lg:ml-[10rem] lg:min-w-[800px] lg:grow'>
 
      {/* { (router.pathname==="/search" || router?.pathname==="/login") ? null: <div className='py-1 md:rounded-[10px] head' style={{textAlign:"center"}}>Y.A.P.S [Yet Another Piracy Site]</div>} */}
-        <br/>
+       
         {children}
         <Footer />
       </main>
