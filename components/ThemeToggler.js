abcf48ebec5572e287e51c4d0ac604f5a5617195
Rohithk <67067796+Rohithk2003@users.noreply.github.com>
2023-09-22 10:18:37 +0530

    fixed dark mode button

--- components/ThemeToggler.js
+++ components/ThemeToggler.js
@@ -2,38 +2,47 @@ import { useEffect, useState } from 'react'
 import { useTheme } from 'next-themes'
 
 export function ThemeToggler() {
+  const [mounted, setMounted] = useState(false)
   const { theme, setTheme } = useTheme()
   const circleBtn = 'rotate-[360deg] '
   const [test, setest] = useState(true)
   useEffect(() => {
-    document.querySelector('.dark-mode-btn').addEventListener('click', () => {
-      document
-        .querySelector('.dark-mode-btn')
-        .classList.toggle('rotate-[360deg]')
-    })
+    setMounted(true)
+    setTheme('dark')
   }, [])
-  return (
-    <div
-      className={`absolute right-10 top-36 rounded-3xl p-2 lg:right-5 lg:top-5 ${
-        theme === 'dark' ? 'bg-app-pure-white' : 'bg-app-semi-dark-blue'
-      } h-12 w-12 text-white`}>
-      <div className={'h-full w-full rounded-3xl'}>
-        <div
-          onClick={() => {
-            setTheme(theme === 'light' ? 'dark' : 'light')
-            setest(false)
-          }}
-          className={`${
-            test === false ? circleBtn : 'circleBtn'
-          } dark-mode-btn flex h-full w-full cursor-pointer items-center justify-center rounded-full transition  duration-500 ease-in-out `}>
-          <span
-            className={` ${
-              theme === 'dark' ? 'text-black' : ''
-            } material-symbols-outlined`}>
-            {theme === 'dark' ? 'light_mode' : 'dark_mode'}
-          </span>
+  useEffect(() => {
+    if (mounted) {
+      document.querySelector('.dark-mode-btn').addEventListener('click', () => {
+        document
+          .querySelector('.dark-mode-btn')
+          .classList.toggle('rotate-[360deg]')
+      })
+    }
+  }, [mounted])
+  if (!mounted) {
+    return null
+  } else {
+    return (
+      <div
+        className={`absolute right-10 top-36 rounded-3xl p-2 lg:right-5 lg:top-5 ${
+          theme !== 'dark' ? 'bg-app-semi-dark-blue' : 'bg-app-pure-white'
+        } h-12 w-12 text-white`}>
+        <div className={'h-full w-full rounded-3xl'}>
+          <div
+            onClick={() => {
+              setTheme(theme === 'light' ? 'dark' : 'light')
+              setest(false)
+            }}
+            className={`dark-mode-btn flex h-full w-full cursor-pointer items-center justify-center rounded-full transition  duration-500 ease-in-out `}>
+            <span
+              className={` ${
+                theme === 'dark' ? 'text-black' : ''
+              } material-symbols-outlined`}>
+              {theme === 'dark' ? 'light_mode' : 'dark_mode'}
+            </span>
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
