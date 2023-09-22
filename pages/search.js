0afd57f580753d1b704ecb022817a87fa45f5135
Rohithk <67067796+Rohithk2003@users.noreply.github.com>
2023-09-22 11:50:05 +0530

    added theme switching for movie page

--- pages/search.js
+++ pages/search.js
@@ -1,173 +1,204 @@
 import CardCompact from '../Common/CardCompact'
 import CardExpanded from '../Common/CardExpanded'
-import {useRouter} from 'next/router'
-import React, {useState} from 'react'
-import {MovieSVG, SearchSVG} from '../SVG/search'
-import {getSearchResult} from '../service/service';
-import {Loader} from '../Common/Loader'
-import {CompactList, ExpandedList} from '../SVG/listing'
+import { useRouter } from 'next/router'
+import React, { useState, useEffect } from 'react'
+import { MovieSVG, SearchSVG } from '../SVG/search'
+import { getSearchResult } from '../service/service'
+import { Loader } from '../Common/Loader'
+import { CompactList, ExpandedList } from '../SVG/listing'
 import ToastMsg from '../Common/ToastMsg'
 import Head from 'next/head'
-
-let data1 = [{name: "Movie", cat: "Movies", time: "10D"}, {name: "TV-Show", cat: "TV", time: "10D"}, {
-    name: "Games",
-    cat: "Games",
-    time: "10D"
-}, {name: "Music", cat: "Music", time: "10D"}, {name: "Anime", cat: "Anime", time: "10D"}, {
-    name: "Books",
-    cat: "Books",
-    time: "10D"
-}, {name: "Other", cat: "Other", time: "10D"}, {name: "XXX", cat: "XXX", time: "1D"}]
-
+import { useTheme } from 'next-themes'
+let data1 = [
+  { name: 'Movie', cat: 'Movies', time: '10D' },
+  { name: 'TV-Show', cat: 'TV', time: '10D' },
+  {
+    name: 'Games',
+    cat: 'Games',
+    time: '10D',
+  },
+  { name: 'Music', cat: 'Music', time: '10D' },
+  { name: 'Anime', cat: 'Anime', time: '10D' },
+  {
+    name: 'Books',
+    cat: 'Books',
+    time: '10D',
+  },
+  { name: 'Other', cat: 'Other', time: '10D' },
+  { name: 'XXX', cat: 'XXX', time: '1D' },
+]
 
 const Home = () => {
-    const router = useRouter()
+  const router = useRouter()
 
-    const [movieList, setMovieList] = useState([])
-    const [search, setSearch] = useState(false)
-    const [loader, setLoader] = useState(false)
-    const [ListType, setListType] = useState('expanded');
-    const [searchSuccess, setSearchSuccess] = useState(false)
-    const [checkboxInput, setCheckboxInput] = useState([])
-    const [searchbarclicked, setSearchBar] = useState(false)
-    const searchResult = (e) => {
-        e.preventDefault()
-        setLoader(true)
-        getSearchResult(search, checkboxInput).then((res) => {
-            setSearchSuccess(true)
-            console.log("page", res?.data?.results)
-            setLoader(false)
-            setMovieList(res.data.results)
-        }).catch((err) => {
-            console.log("error", err)
+  const [movieList, setMovieList] = useState([])
+  const [search, setSearch] = useState(false)
+  const [loader, setLoader] = useState(false)
+  const [ListType, setListType] = useState('expanded')
+  const [searchSuccess, setSearchSuccess] = useState(false)
+  const [checkboxInput, setCheckboxInput] = useState([])
+  const [searchbarclicked, setSearchBar] = useState(false)
+  const { theme, setTheme } = useTheme()
+  useEffect(() => {
+    setTheme(theme)
+  })
+  const searchResult = e => {
+    e.preventDefault()
+    setLoader(true)
+    getSearchResult(search, checkboxInput)
+      .then(res => {
+        setSearchSuccess(true)
+        console.log('page', res?.data?.results)
+        setLoader(false)
+        setMovieList(res.data.results)
+      })
+      .catch(err => {
+        console.log('error', err)
 
-            ToastMsg("Some thing went wrong ", "error")
-            setLoader(false)
-        })
-
+        ToastMsg('Some thing went wrong ', 'error')
+        setLoader(false)
+      })
+  }
+  const handleCheckboxChange = event => {
+    const { value, checked } = event.target
+    console.log('event', checked, value)
+    if (checked) {
+      setCheckboxInput([...checkboxInput, value])
+    } else {
+      setCheckboxInput(checkboxInput.filter(item => item !== value))
     }
-    const handleCheckboxChange = (event) => {
-        const {value, checked} = event.target;
-        console.log("event", checked, value)
-        if (checked) {
-            setCheckboxInput([...checkboxInput, value]);
-        } else {
-            setCheckboxInput(checkboxInput.filter((item) => item !== value));
-        }
-    };
+  }
 
-
-    return (
-        <div
-            className="container  mx-auto py-3 font-light bg-transparent min-h-screen  justify-center text-center font-montserrat">
-            <Head>
-                <title>Register | Yaps</title>
-            </Head>
-            {loader ? <Loader/> : null}
-            <div>
-                <p className='text-[3rem] md:text-[6rem] font-bold leading-[3.5rem] md:leading-[7rem] pt-16'> This
-                    World.<br/>At Your Fingertips.</p>
-                <div>
-
-                    <form onSubmit={(e) => searchResult(e)}
-                          className={`border-b-[1.5px] w-10/12 md:w-1/2 mx-auto flex my-10 items-center justify-center  border-primary px-1`}>
-                        <input
-                            className='bg-transparent w-full py-4 font-light text-lg outline-none dark:placeholder:text-app-pure-white placeholder:text-black placeholder:font-montserrat font-montserrat'
-                            placeholder='Start typing what you want ?' onChange={(e) => setSearch(e.target.value)}/>
-                        <div onClick={(e) => {
-                            searchResult(e)
-                        }}>
-                            <SearchSVG/>
-                        </div>
-                    </form>
-                    <div className='mx-8 flex text-center justify-center'>
-                        <div className='font-thin flex flex-wrap items-center flex-row justify-evenly'>
-                            {
-                                data1.map((item, index) => {
-                                    return (
-                                        <label key={index} className="checkbox">
-                                            <input type="checkbox" name={item?.name} value={item?.cat}
-                                                   className='w-4 h-4 rounded checked:bg-primary dark:checked:border-primary border border-blue-800 dark:border-primary'
-                                                   onChange={handleCheckboxChange}
-                                                   checked={checkboxInput?.includes(item?.cat)}/>
-                                            <span
-                                                className={"dark:!text-app-pure-white !text-app-dark-blue  "}>{item?.name}</span>
-                                        </label>
-
-                                    )
-                                })
-                            }
-                        </div>
-                    </div>
-                    <br/>
-
-                    {searchSuccess ?
-                        <div className='flex justify-center m-2 w-[98%] relative'>
-                            <div className=" bg-off-white/10 w-[64%] rounded-xl  justify-center align-middle p-2">
-                                {movieList.length > 0 ? <span>Your Search Result</span> :
-                                    <span>No data found for : "{search}"</span>}
-                            </div>
-
-                            {movieList.length > 0 ? <div className='flex justify-end absolute right-0'>
-                                    <div
-                                        className={`px-4 py-[0.35rem] ${ListType === 'compact' ? 'text-primary bg-primary/30' : ''} rounded-xl cursor-pointer transition-all duration-200`}
-                                        onClick={() => {
-                                            setListType('compact')
-                                        }}><CompactList/></div>
-                                    <div
-                                        className={`px-4 py-[0.35rem] ${ListType === 'expanded' ? 'text-primary bg-primary/30' : ''} rounded-xl cursor-pointer transition-all duration-200`}
-                                        onClick={() => {
-                                            setListType('expanded')
-
-                                        }}><ExpandedList/></div>
-                                </div>
-                                : null}
-
-
-                        </div> : null}
-                    <div className={`flex-1 flex-wrap pl-6 py-8 justify-center`}>
-                        {movieList?.map((item, index) => {
-                            if (ListType === 'compact') {
-                                return (
-                                    <CardCompact key={index} item={item}/>
-                                )
-                            } else {
-                                return (
-                                    <CardExpanded key={index} item={item}/>
-                                )
-                            }
-                        })}
-
-                    </div>
-
-                    <br/>
-
-                    <div className='md:px-0 w-full md:w-8/12 mx-auto text-center flex flex-wrap gap-4 justify-center'>
-                        {
-                            data1.map((item, index) => {
-                                return (
-                                    <div
-                                        className='relative dark:bg-app-dark-blue bg-[#f3f3f3] dark:shadow-[0px] shadow-2xl'
-                                        key={index}>
-                                        <div key={index}
-                                             onClick={() => window.location.href = `/get-posts/category:${item?.cat}?time=${item?.time}`}
-                                             className={`cursor-pointer w-[150px] md:w-[195px] h-[115px] md:pl-[26px] md:pr-[161px] pt-7 pb-16 bg-app-dark-blue dark:bg-off-white/10 rounded-lg border-[0] dark:border-off-white/30 flex-col justify-start items-center md:items-start gap-3.5 inline-flex hover:bg-[#18141f] hover:scale-[1.05] transition duration-300 ease-in-out dark:hover:bg-primary/10 dark:hover:border-primary/50 group `}>
-                                            <div
-                                                className="w-[30px] h-[30px] p-[2.50px] justify-center items-center inline-flex text-off-white group-hover:text-primary">
-                                                <MovieSVG/></div>
-                                            <div
-                                                className="text-off-white hover:text-app-dark-blue dark:group-hover:text-primary text-opacity-80 text-[13px] w-20 text-center md:text-start leading-[0px] ">{item?.name}</div>
-                                        </div>
-                                    </div>
-                                )
-                            })}
-
-
-                    </div>
-                </div>
+  return (
+    <div className='font-montserrat  container mx-auto min-h-screen justify-center bg-transparent  py-3 text-center font-light'>
+      <Head>
+        <title>Register | Yaps</title>
+      </Head>
+      {loader ? <Loader /> : null}
+      <div>
+        <p className='pt-16 text-[3rem] font-bold leading-[3.5rem] md:text-[6rem] md:leading-[7rem]'>
+          {' '}
+          This World.
+          <br />
+          At Your Fingertips.
+        </p>
+        <div>
+          <form
+            onSubmit={e => searchResult(e)}
+            className={`mx-auto my-10 flex w-10/12 items-center justify-center border-b-[1.5px] border-primary  px-1 md:w-1/2`}>
+            <input
+              className='placeholder:font-montserrat font-montserrat w-full bg-transparent py-4 text-lg font-light outline-none placeholder:text-black dark:placeholder:text-app-pure-white'
+              placeholder='Start typing what you want ?'
+              onChange={e => setSearch(e.target.value)}
+            />
+            <div
+              onClick={e => {
+                searchResult(e)
+              }}>
+              <SearchSVG />
             </div>
+          </form>
+          <div className='mx-8 flex justify-center text-center'>
+            <div className='flex flex-row flex-wrap items-center justify-evenly font-thin'>
+              {data1.map((item, index) => {
+                return (
+                  <label key={index} className='checkbox'>
+                    <input
+                      type='checkbox'
+                      name={item?.name}
+                      value={item?.cat}
+                      className='h-4 w-4 rounded border border-blue-800 checked:bg-primary dark:border-primary dark:checked:border-primary'
+                      onChange={handleCheckboxChange}
+                      checked={checkboxInput?.includes(item?.cat)}
+                    />
+                    <span
+                      className={
+                        '!text-app-dark-blue dark:!text-app-pure-white  '
+                      }>
+                      {item?.name}
+                    </span>
+                  </label>
+                )
+              })}
+            </div>
+          </div>
+          <br />
+
+          {searchSuccess ? (
+            <div className='relative m-2 flex w-[98%] justify-center'>
+              <div className=' w-[64%] justify-center rounded-xl  bg-off-white/10 p-2 align-middle'>
+                {movieList.length > 0 ? (
+                  <span>Your Search Result</span>
+                ) : (
+                  <span>No data found for : "{search}"</span>
+                )}
+              </div>
+
+              {movieList.length > 0 ? (
+                <div className='absolute right-0 flex justify-end'>
+                  <div
+                    className={`px-4 py-[0.35rem] ${
+                      ListType === 'compact' ? 'bg-primary/30 text-primary' : ''
+                    } cursor-pointer rounded-xl transition-all duration-200`}
+                    onClick={() => {
+                      setListType('compact')
+                    }}>
+                    <CompactList />
+                  </div>
+                  <div
+                    className={`px-4 py-[0.35rem] ${
+                      ListType === 'expanded'
+                        ? 'bg-primary/30 text-primary'
+                        : ''
+                    } cursor-pointer rounded-xl transition-all duration-200`}
+                    onClick={() => {
+                      setListType('expanded')
+                    }}>
+                    <ExpandedList />
+                  </div>
+                </div>
+              ) : null}
+            </div>
+          ) : null}
+          <div className={`flex-1 flex-wrap justify-center py-8 pl-6`}>
+            {movieList?.map((item, index) => {
+              if (ListType === 'compact') {
+                return <CardCompact key={index} item={item} />
+              } else {
+                return <CardExpanded key={index} item={item} />
+              }
+            })}
+          </div>
+
+          <br />
+
+          <div className='mx-auto flex w-full flex-wrap justify-center gap-4 text-center md:w-8/12 md:px-0'>
+            {data1.map((item, index) => {
+              return (
+                <div
+                  className='relative bg-[#f3f3f3] shadow-2xl dark:bg-app-dark-blue dark:shadow-[0px]'
+                  key={index}>
+                  <div
+                    key={index}
+                    onClick={() =>
+                      (window.location.href = `/get-posts/category:${item?.cat}?time=${item?.time}`)
+                    }
+                    className={`group inline-flex h-[115px] w-[150px] cursor-pointer flex-col items-center justify-start gap-3.5 rounded-lg border-[0] bg-app-dark-blue pb-16 pt-7 transition duration-300 ease-in-out hover:scale-[1.05] hover:bg-[#18141f] hover:text-white dark:border-off-white/30 dark:bg-off-white/10 dark:hover:border-primary/50 dark:hover:bg-primary/10 md:w-[195px] md:items-start md:pl-[26px] md:pr-[161px] `}>
+                    <div className='inline-flex h-[30px] w-[30px] items-center justify-center p-[2.50px] text-off-white group-hover:text-primary'>
+                      <MovieSVG />
+                    </div>
+                    <div className='w-20 text-center text-[13px] leading-[0px] text-off-white text-opacity-80 hover:text-app-pure-white dark:hover:text-app-dark-blue dark:group-hover:text-primary md:text-start '>
+                      {item?.name}
+                    </div>
+                  </div>
+                </div>
+              )
+            })}
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default Home
