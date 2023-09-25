ef9aaec9da427fade987bc7d7c31173cb4290ca3
Rohithk <67067796+Rohithk2003@users.noreply.github.com>
2023-09-25 23:50:43 +0530

    removed a file

--- Common/TorrentList.js
+++ Common/TorrentList.js
@@ -1,183 +1,184 @@
-import moment from 'moment';
-import {useRouter} from 'next/router';
+import moment from 'moment'
+import { useRouter } from 'next/router'
 import React from 'react'
 
-import Modal from "react-modal";
+import Modal from 'react-modal'
 
-import {formatBytes} from './CardExpanded';
+import { formatBytes } from './CardExpanded'
 
-import DataTable, {createTheme} from 'react-data-table-component';
-import {useTheme} from "next-themes";
+import DataTable, { createTheme } from 'react-data-table-component'
+import { useTheme } from 'next-themes'
 
-const TorrentList = ({setisTorrent, torrent_list, runtime}) => {
-    const {theme, settheme} = useTheme();
-    const router = useRouter()
+const TorrentList = ({ setisTorrent, torrent_list, runtime }) => {
+  const { theme, settheme } = useTheme()
+  const router = useRouter()
 
-    const customStyles = {
+  const customStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+      background: '#171e30',
+      maxHeight: '700px',
+      maxWidth: '1500px',
+      border: '0px',
+      width: '100%',
+      borderColor: '#454851',
+    },
+  }
 
-        content: {
-            top: '50%',
-            left: '50%',
-            right: 'auto',
-            bottom: 'auto',
-            marginRight: '-50%',
-            transform: 'translate(-50%, -50%)',
-            background: "#171e30",
-            maxHeight: "700px",
-            maxWidth: "1500px",
-            border:'0px',
-            width: "100%",
-            borderColor: "#454851",
-        },
+  const qualityRegex = /(4K|720p|1080p|2160p|720P|360P|360p|1080P)/i
 
-    };
+  const columns = [
+    {
+      name: 'Name',
+      selector: row => row.name,
+      sortable: true,
+    },
+    {
+      name: 'Action',
 
-    const qualityRegex = /(4K|720p|1080p|2160p|720P|360P|360p|1080P)/i;
+      cell: (row, index) => {
+        let slug = row?.name
+          .toLowerCase()
+          .trim()
+          .replace(/[^\w\s-]/g, '')
+          .replace(/[\s_-]+/g, '-')
+          .replace(/^-+|-+$/g, '')
 
-    const columns = [
-        {
-            name: 'Name',
-            selector: row => row.name,
-            sortable: true,
+        return (
+          <a onClick={() => router.push(`/post-detail/${row?.eid}/${slug}/`)}>
+            Download
+          </a>
+        )
+      },
+      width: '8%',
+      style: {
+        color: '#296ac8',
+        cursor: 'pointer',
+      },
+    },
+    {
+      name: 'Category',
+      selector: row => row.category_str,
+      width: '8%',
+      sortable: true,
+    },
+    {
+      name: 'Res',
+      selector: row =>
+        row.name.match(qualityRegex) ? row.name.match(qualityRegex)[0] : '',
+      width: '6%',
+      sortable: true,
+      sortFunction: (a, b) => {
+        function compareQuality(a, b) {
+          if (a && b) {
+            const regex = /(\d+)p/
+            const qualityA = parseInt(
+              a.name.match(qualityRegex)
+                ? a.name.match(qualityRegex)[0]?.match(regex)
+                : ''
+            )
+            const qualityB = parseInt(
+              b.name.match(qualityRegex)
+                ? b.name.match(qualityRegex)[0]?.match(regex)
+                : ''
+            )
+            return qualityA - qualityB
+          }
+        }
 
-        },
-        {
-            name: 'Action',
+        return compareQuality(a, b)
+      },
+    },
+    {
+      name: 'Date',
+      selector: row => moment(row?.last_checked).format('DD-MM-YYYY'),
+      width: '9%',
+      sortable: true,
+    },
+    {
+      name: 'Runtime',
+      selector: row => moment.utc(runtime * 1000).format('HH:mm:ss'),
+      width: '9%',
+      sortable: true,
+    },
+    {
+      name: 'Size',
+      selector: row => formatBytes(row?.size),
+      sortable: true,
+      width: '7%',
+      sortFunction: (a, b) => {
+        return a.size - b.size
+      },
+    },
+    {
+      name: 'S',
+      selector: row => row?.seeders,
+      sortable: true,
+      width: '5%',
+      style: {
+        color: '#00FF00',
+      },
+    },
+    {
+      name: 'L',
+      selector: row => row.leechers,
+      sortable: true,
+      width: '5%',
+      style: {
+        color: '#dd0c0e',
+      },
+    },
+  ]
+  createTheme('dark', {
+    background: {
+      default: 'transparent',
+    },
+  })
 
-            cell: (row, index) => {
-                let slug = row?.name.toLowerCase().trim().replace(/[^\w\s-]/g, '').replace(/[\s_-]+/g, '-').replace(/^-+|-+$/g, '');
+  const customCss = {
+    headCells: {
+      style: {
+        fontSize: '14px',
+        fontWeight: '700',
+        color: theme === 'dark' ? '#C2D8D3' : '#000000',
+      },
+    },
+  }
 
-                return (
-                    <a onClick={() => router.push(`/post-detail/${row?.eid}/${slug}/`)}>
-                        Download
-                    </a>
-                )
-            },
-            width: "8%",
-            style: {
-                color: "#296ac8",
-                cursor: "pointer",
+  console.log('torrent_list', torrent_list)
 
-            }
-        },
-        {
-            name: 'Category',
-            selector: row => row.category_str,
-            width: "8%",
-            sortable: true,
-        },
-        {
-            name: "Res",
-            selector: row => row.name.match(qualityRegex) ? row.name.match(qualityRegex)[0] : '',
-            width: '6%',
-            sortable: true,
-            sortFunction: (a, b) => {
-                function compareQuality(a, b) {
-                    if (a && b) {
-                        const regex = /(\d+)p/;
-                        const qualityA = parseInt(a.name.match(qualityRegex) ? a.name.match(qualityRegex)[0]?.match(regex): '');
-                        const qualityB = parseInt(b.name.match(qualityRegex) ? b.name.match(qualityRegex)[0]?.match(regex) : '');
-                        return qualityA - qualityB;
-                    }
-                }
+  createTheme('dark', {
+    background: {
+      default: 'transparent',
+    },
+  })
 
-                return compareQuality(a, b)
-            },
-        },
-        {
-            name: 'Date',
-            selector: row => moment(row?.last_checked).format("DD-MM-YYYY"),
-            width: "9%",
-            sortable: true,
-        },
-        {
-            name: 'Runtime',
-            selector: row => moment.utc(runtime * 1000).format('HH:mm:ss'),
-            width: "9%",
-            sortable: true,
-        },
-        {
-            name: 'Size',
-            selector: row => formatBytes(row?.size),
-            sortable: true,
-            width: "7%",
-            sortFunction: (a, b) => {
-
-                return a.size - b.size;
-            },
-        },
-        {
-            name: 'S',
-            selector: row => row?.seeders,
-            sortable: true,
-            width: "5%",
-            style: {
-                color: "#00FF00",
-            }
-        },
-        {
-            name: 'L',
-            selector: row => row.leechers,
-            sortable: true,
-            width: "5%",
-            style: {
-                color: "#dd0c0e",
-
-            }
-        },
-    ];
-    createTheme('dark', {
-        background: {
-            default: 'transparent',
-        },
-    });
-
-    const customCss = {
-
-        headCells: {
-            style: {
-                fontSize: "14px",
-                fontWeight: "700",
-                color: theme === "dark" ? "#C2D8D3" : "#000000",
-            },
-        },
-
-    };
-
-
-    console.log("torrent_list", torrent_list)
-
-    createTheme('dark', {
-        background: {
-            default: 'transparent',
-        },
-
-    });
-
-
-    return (
-
-        <>
-            <Modal
-                isOpen={true}
-                style={customStyles}
-                onRequestClose={() => setisTorrent(false)}
-            >  {
-                torrent_list.length > 0 ?
-                    <DataTable
-                        columns={columns}
-                        data={torrent_list}
-                        theme={theme}
-                        sortable
-                        customStyles={customCss}
-                    />
-
-                    : <div className='nosimilar_torrent text-gray-700 '>
-                        <h1>
-                            Sorry Torrent Not Available Try after some days !!!
-                        </h1>
-                    </div>}
-                {/* {
+  return (
+    <>
+      <Modal
+        isOpen={true}
+        style={customStyles}
+        onRequestClose={() => setisTorrent(false)}>
+        {' '}
+        {torrent_list.length > 0 ? (
+          <DataTable
+            columns={columns}
+            data={torrent_list}
+            theme={theme}
+            sortable
+            customStyles={customCss}
+          />
+        ) : (
+          <div className='nosimilar_torrent text-gray-700 '>
+            <h1>Sorry Torrent Not Available Try after some days !!!</h1>
+          </div>
+        )}
+        {/* {
 
           torrent_list.length>0?
 
@@ -281,15 +282,9 @@ const TorrentList = ({setisTorrent, torrent_list, runtime}) => {
          </h1>
 
        </div> } */}
-
-
-            </Modal>
-
-        </>
-
-    )
-
+      </Modal>
+    </>
+  )
 }
-
 
 export default TorrentList
